979244d1a510d14af99af144cf5c90675cb7152f
chiameng.ong <chiameng.ong@SIC-SSLT189.sicmsb.com>
2019-06-19 10:53:51 +0800

    Adding endpoint test case

--- httpbasic/src/test/java/com/example/demo/SslEndpointTest.java
+++ httpbasic/src/test/java/com/example/demo/SslEndpointTest.java
@@ -1,85 +1,38 @@
 package com.example.demo;
 
-import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.util.Collections;
-
-import javax.net.ssl.SSLContext;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SslEndpointTest {
 
 	@Value("${server.ssl.trust.store}")
-    private Resource keyStore;
-    @Value("${server.ssl.trust.store.password}")
-    private String keyStorePassword;
-	
+	private Resource keyStore;
+	@Value("${server.ssl.trust.store.password}")
+	private String keyStorePassword;
+
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	public void testDemoEndpoint() {
-		try {
-			ResponseEntity<String> response = restTemplate2().getForEntity("https://localhost:8080/demo", String.class,
-					Collections.emptyMap());
-
-			assertEquals("HTTP Basic Authentication working...", response.getBody());
-			assertEquals(HttpStatus.OK, response.getStatusCode());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private RestTemplate restTemplate2() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException,
-			CertificateException, IOException {
-		SSLContext sslContext = new SSLContextBuilder()
-				.loadTrustMaterial(keyStore.getURL(), keyStorePassword.toCharArray()).build();
-		SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
-		HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
-		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-		return new RestTemplate(factory);
-	}
-
-	private RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException,
-			CertificateException, FileNotFoundException, IOException, KeyManagementException {
-		KeyStore clientStore = KeyStore.getInstance("PKCS12");
-		clientStore.load(new FileInputStream("C:/workspace/httpbasic/keystore/sb2.p12"), "123456".toCharArray());// keystore
-																													// password
-
-		SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
-		sslContextBuilder.useProtocol("TLS");
-		sslContextBuilder.loadKeyMaterial(clientStore, "123456".toCharArray());// keystore password
-		sslContextBuilder.loadTrustMaterial(new TrustSelfSignedStrategy());
-
-		SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
-				sslContextBuilder.build());
-		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
-		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-		requestFactory.setConnectTimeout(10000); // 10 seconds
-		requestFactory.setReadTimeout(10000); // 10 seconds
-		return new RestTemplate(requestFactory);
+	public void testEndpoint() throws Exception {
+		mvc.perform(get("/demo").secure(true).header("Authorization", "Basic YWRtaW46MTIz"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/text;charset=UTF-8"))
+				.andExpect(content().string("HTTP Basic Authentication working..."));
 	}
 
 }
